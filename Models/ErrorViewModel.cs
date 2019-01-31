2005828d807f6c8e29a3c436ccaec20ea0483d70
trev-cc <fleemant@gmail.com>
2019-01-30 21:10:47 -0600

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CodeClub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
